89d9ee40583a0f93a15fa320a9c779652accbb63
Evandro Carreira <evandro.carreira@gmail.com>
2015-01-11 21:52:24 -0200

    Remove Packery, Css puro

--- index.php
+++ index.php
@@ -8,46 +8,64 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <link rel="icon" href="../img/favicon-16.png" sizes="16x16">
-    <link rel="icon" href="../img/favicon-32.png" sizes="32x32">
+    <link rel="icon" href="img/favicon-16.png" sizes="16x16">
+    <link rel="icon" href="img/favicon-32.png" sizes="32x32">
 
     <title>Neurônio Produtora</title>
 
-    <script src="plugins/packery.min.js"></script>
     
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
-<div id="container" class="js-packery"  data-packery-options='{ "itemSelector": ".video", "gutter": 0 , "stamp": ".logo"}'>
-  <div class="video azul"></div>
-  <div class="video vermelho"></div>
-  <div class="video"></div>  
-    <div class="video verde"></div>
+    <div id="container"  >
+        <div class="video logo"><img src="/img/logo.png" alt="Neurônio Produtora"></div>
 
-  <div class="video"></div>  <div class="video logo"><img src="/img/logo.png" alt="Neurônio Produtora"></div>
+        <div class="video azul"></div>
+        <div class="video verde"></div>
 
-  <div class="video azul"></div>
-  <div class="video magenta"></div>
-  <div class="video "></div>
-  <div class="video verde"></div>
-  <div class="video "></div>
-  <div class="video vermelho"></div>
-  <div class="video azul"></div>
-  <div class="video magenta"></div>
-  <div class="video vermelho"></div>
-  <div class="video azul"></div>
-  <div class="video verde"></div>
-  <div class="video vermelho"></div>
-  <div class="video azul"></div>
-  <div class="video vermelho"></div>
-  <div class="video azul"></div>
-</div>
-    
-    
-    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <div class="video only-small"></div>  
+        <div class="video only-small"></div>  
+        <div class="video only-small"></div>   
+        <div class="video only-small"></div>  
+        
+        <div class="video vermelho"></div>   
+        
+        
+        <div class="video only-medium"></div>  
+        <div class="video only-medium"></div>  
+        <div class="video only-medium"></div>   
+        <div class="video only-medium"></div>  
+        <div class="video only-medium"></div>   
+        <div class="video only-medium"></div>  
+        
+        <div class="video magenta"></div> 
+        <div class="video verde"></div>  
+        
+        <div class="video only-big"></div>  
+        <div class="video only-big"></div>  
+        
+        <div class="video "></div>  
+        <div class="video azul"></div>
+        
+        <div class="video only-big"></div>  
+        <div class="video only-big"></div> 
+        
+        <div class="video vermelho"></div>
+        <div class="video magenta"></div>
+        <div class="video verde"></div>
+        <div class="video "></div>
+        <div class="video azul"></div>
+        <div class="video vermelho"></div>
+        <div class="video magenta"></div>
+        <div class="video verde"></div>
+        <div class="video "></div>
+    </div>
 
+
+    <script src="plugins/jquery.min.js"></script>
+ 
 </body>
 
 </html>
